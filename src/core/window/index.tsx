379d50d283ef8749e9f5ab534888c68938d83e34
Rodrigo de França <howdrigo.fr@gmail.com>
2022-04-21 18:12:51 -0300

    refactor: remove store

--- src/core/window/index.tsx
+++ src/core/window/index.tsx
@@ -1,89 +1,10 @@
 import { Observer } from 'mobx-react-lite'
-import { makeAutoObservable, runInAction } from 'mobx'
-import * as Styled from './styles'
 import { BsX, BsDash } from 'react-icons/bs'
 import { BiRectangle } from 'react-icons/bi'
-import taskManager  from '../taskManager'
 
+import { WindowStore } from './store'
+import * as Styled from './styles'
 
-class WindowStore {
-  dragging = false
-  mouseOffset = { x: 0, y: 0 }
-  containerPosition = { x: 0, y: 0 }
-  container: HTMLDivElement | null = null
-  isFocused = false
-  isMaximized = false
-  isMinimized = false
-  uuid = window.crypto.randomUUID()
-
-  titleBar = true
-  minimizeButton = true
-  maximizeButton = true
-  requestToClose = false
-
-  constructor() {
-    makeAutoObservable(this, {}, {
-      autoBind: true
-    })
-  }
-
-  setIsFocused(isFocused: boolean) {
-    this.isFocused = isFocused
-  }
-
-  setIsMaximized(isMaximized: boolean) {
-    this.isMaximized = isMaximized
-  }
-
-  setIsMinimized(isMinimized: boolean) {
-    this.isMinimized = isMinimized
-  }
-
-  setMaximizeButton(maximizeButton: boolean) {
-    this.maximizeButton = maximizeButton
-  }
-
-  setRequestToClose(requestToClose: boolean) {
-    this.requestToClose = requestToClose
-  }
-
-  setMinimizeButton(minimizeButton: boolean) {
-    this.minimizeButton = minimizeButton
-  }
-
-  setContainerRef(ref: HTMLDivElement | null) {
-    this.container = ref
-  }
-
-  setDragging(dragging: boolean) {
-    this.dragging = dragging
-  }
-
-  onMouseDown(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
-    this.setDragging(true)
-    this.mouseOffset.x = e.clientX
-    this.mouseOffset.y = e.clientY
-    this.containerPosition.x = this.container?.offsetLeft ?? 0
-    this.containerPosition.y = this.container?.offsetTop ?? 0
-
-    taskManager.setFocus(this.uuid)
-  }
-
-  onMouseUp(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
-    this.setDragging(false)
-  }
-
-  onMouseMove(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
-    if (this.dragging) {
-      this.container!.style.left = `${(e.clientX + this.containerPosition.x) - this.mouseOffset.x}px`
-      this.container!.style.top = `${(e.clientY + this.containerPosition.y) - this.mouseOffset.y}px`
-    }
-  }
-
-  onMouseLeave(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
-    this.setDragging(false)
-  }
-}
 
 export class WindowApp {
   store = new WindowStore()
@@ -99,9 +20,7 @@ export class WindowApp {
   close() {
     this.store.setRequestToClose(true)
     setTimeout(() => {
-      runInAction(() => {
-        taskManager.removeApp(this.store.uuid)
-      })
+      this.store.closeApp()
     }, 300)
   }
 
