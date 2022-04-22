abd50637ee25373c24fb1fbd5dac64a8adf4ddbf
Rodrigo de França <howdrigo.fr@gmail.com>
2022-04-22 05:26:46 -0300

    refactor: remove Obsever component

--- src/components/Clock/index.tsx
+++ src/components/Clock/index.tsx
@@ -1,20 +1,14 @@
-import { Observer } from 'mobx-react-lite'
+import { observer } from 'mobx-react-lite'
 import * as Styled from './styles'
 import clockStore from './store'
 
-export function Clock() {
-  return (
-    <Observer>
-      {() => {
-        const [date, hours] = clockStore.getHours()
+export const Clock = observer(() => {
+  const [date, hours] = clockStore.getHours()
 
-        return (
-          <Styled.Container>
-            <Styled.Text>{hours}</Styled.Text>
-            <Styled.Text>{date}</Styled.Text>
-          </Styled.Container>
-        )
-      }}
-    </Observer>
+  return (
+    <Styled.Container>
+      <Styled.Text>{hours}</Styled.Text>
+      <Styled.Text>{date}</Styled.Text>
+    </Styled.Container>
   )
-}
+})
